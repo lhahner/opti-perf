fe189a85970597bdddc0ddf4cb9fa9cb99c14adf
lhahner <lennart.hahner@gmail.com>
2025-11-20 22:59:51 +0100

    removes make file

--- build/speedup/linear_operations.d
+++
@@ -1,4 +0,0 @@
-build/speedup/linear_operations.o: src/speedup/linear_operations.cpp \
- include/speedup/linear_operations.h include/speedup/operations.h
-include/speedup/linear_operations.h:
-include/speedup/operations.h:
